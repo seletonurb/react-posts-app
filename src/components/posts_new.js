2bf9d934ac0dedcabf435b3ff684149babfe896d
Bruno <seletonurb@gmail.com>
2018-08-02 17:41:13 -0300

    Use Field component to render Redux Form fields

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,12 +1,47 @@
 import React, { Component } from "react";
-import { reduxForm } from "redux-form";
+import { Field, reduxForm } from "redux-form";
 
 class PostsNew extends Component {
+  renderTitleField(field) {
+    return (
+      <div className="form-group">
+        <label>Title</label>
+        <input
+          className="form-control"
+          type="text"
+          // All the following event handlers are equivalent to the spread operator below
+          // onChange={field.input.onChange}
+          // onFocus={field.input.onFocus}
+          // onBlur={field.input.onBlur}
+          {...field.input}
+        />
+      </div>
+    );
+  }
+  renderCategoriesField(field) {
+    return (
+      <div className="form-group">
+        <label>Categories</label>
+        <input className="form-control" type="text" {...field.input} />
+      </div>
+    );
+  }
+  renderPostContentField(field) {
+    return (
+      <div className="form-group">
+        <label>Post Content</label>
+        <input className="form-control" type="text" {...field.input} />
+      </div>
+    );
+  }
+
   render() {
     return (
-      <div>
-        <h3>New Post</h3>
-      </div>
+      <form>
+        <Field name="title" component={this.renderTitleField} />
+        <Field name="categories" component={this.renderCategoriesField} />
+        <Field name="post_content" component={this.renderPostContentField} />
+      </form>
     );
   }
 }
