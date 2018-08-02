372a446ce8c284f5bc30f08045e692c3982d4ae1
Bruno <seletonurb@gmail.com>
2018-08-02 12:23:12 -0300

    Populate Post index component with data retrieved from Posts reducer

--- README.md
+++ README.md
@@ -10,3 +10,18 @@ Checkout this repo, install dependencies, then start with the following:
 > npm install
 > npm start
 ```
+
+# Posts API
+
+Service created that can be used to create a post
+```
+http://reduxblog.herokuapp.com/api/posts?key=<anyKeyYouWant>
+```
+Payload (POST method):
+```json
+{
+  "title" : " This is a sample Post",
+  "categories" : "IT",
+  "content" : "This is an IT Post Content"
+}
+```
--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import React, { Component } from "react";
 import {connect} from 'react-redux';
 import {fetchPosts} from '../actions';
@@ -7,14 +8,33 @@ class PostsIndex extends Component {
       this.props.fetchPosts();
     }
 
+    renderPosts() {
+      return _.map(this.props.posts, post => {
+        return (
+          <li className="list-group-item" key={post.id}>
+            {post.title}
+          </li>
+        )
+      })
+    }
     render() {
       return (
         <div>
-          Posts Index
+          <h3>
+          Posts
+          <ul className="list-group">
+            {this.renderPosts()}
+          </ul>
+          </h3>
         </div>
       );
     }
 
 }
 
-export default connect(null, { fetchPosts })(PostsIndex);
+function mapStateToProps(state) {
+  return { posts:state.posts};
+
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
