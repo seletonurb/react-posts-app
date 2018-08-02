325cf5763fc236ffc191abcf0c26e3a4aaf3e3d9
Bruno <seletonurb@gmail.com>
2018-08-02 15:11:21 -0300

    Use Link react router to go to a new route

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,40 +1,42 @@
 import _ from "lodash";
 import React, { Component } from "react";
-import {connect} from 'react-redux';
-import {fetchPosts} from '../actions';
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions";
+import { Link } from "react-router-dom";
 
 class PostsIndex extends Component {
-    componentDidMount() {
-      this.props.fetchPosts();
-    }
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
 
-    renderPosts() {
-      return _.map(this.props.posts, post => {
-        return (
-          <li className="list-group-item" key={post.id}>
-            {post.title}
-          </li>
-        )
-      })
-    }
-    render() {
+  renderPosts() {
+    return _.map(this.props.posts, post => {
       return (
-        <div>
-          <h3>
-          Posts
-          <ul className="list-group">
-            {this.renderPosts()}
-          </ul>
-          </h3>
-        </div>
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
       );
-    }
-
+    });
+  }
+  render() {
+    return (
+      <div>
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/posts/new">
+            Add a Post
+          </Link>
+        </div>
+        <h3>
+          Posts
+          <ul className="list-group">{this.renderPosts()}</ul>
+        </h3>
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-  return { posts:state.posts};
-
+  return { posts: state.posts };
 }
 
-export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
+export default connect( mapStateToProps, { fetchPosts })(PostsIndex);
