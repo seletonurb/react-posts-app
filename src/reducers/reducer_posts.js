8b0c1ab377ba47b8cc0c4f25c0e8ffe6b27de65b
Bruno <seletonurb@gmail.com>
2018-08-03 16:16:14 -0300

    Posts show component using ownProps variable, Action Creation and Reducer

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,9 +1,15 @@
 import _ from "lodash";
-import { FETCH_POSTS } from "../actions";
+import { FETCH_POSTS, FETCH_POST } from "../actions";
 
 export default function(state = {}, action) {
-  console.log('reducer ' + FETCH_POSTS);
+  console.log("reducer " + FETCH_POSTS);
   switch (action.type) {
+    case FETCH_POST:
+      // const post = action.payload.data;
+      // const newState = { ...state }; // get all already fetched posts and put them in the state object
+      // newState[post.id] = post;
+      // return newState;
+      return { ...state, [action.payload.data.id] : action.payload.data };
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, "id");
     default:
