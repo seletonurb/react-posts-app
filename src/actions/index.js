88ca9f7788427b82f703a892bcaff3f09febf510
Bruno <seletonurb@gmail.com>
2018-08-03 17:12:35 -0300

    Delete Post (updating posts array after deletion)

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export const FETCH_POSTS = "fetch_posts";
 export const FETCH_POST = "fetch_post";
 export const CREATE_POST = "create_post";
+export const DELETE_POST = "delete_post";
 
 const ROOT_URL = "http://reduxblog.herokuapp.com/api";
 const API_KEY = "?key=LYNX1234";
@@ -18,7 +19,9 @@ export function fetchPosts() {
 }
 
 export function createPost(values, callback) {
-  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values).then(() => callback());
+  const request = axios
+    .post(`${ROOT_URL}/posts${API_KEY}`, values)
+    .then(() => callback());
 
   return {
     type: CREATE_POST,
@@ -32,5 +35,16 @@ export function fetchPost(id) {
   return {
     type: FETCH_POST,
     payload: request
+  };
+}
+
+export function deletePost(id, callback) {
+  const request = axios
+    .delete(`${ROOT_URL}/posts/${id}${API_KEY}`)
+    .then(() => callback());
+
+  return {
+    type: DELETE_POST,
+    payload: id
   };
 }
