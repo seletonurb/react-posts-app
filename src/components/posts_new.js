9e0a3fe8f953ed5ce99ebdce98a0cc560b3ab3d4
Bruno <seletonurb@gmail.com>
2018-08-03 14:28:10 -0300

    Handle Post Creation callback using programmatic navigation (component history props)

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -31,7 +31,9 @@ class PostsNew extends Component {
 
   onSubmit(values) {
     // this === component
-    this.props.createPost(values);
+    this.props.createPost(values, () => {
+      this.props.history.push('/');
+    });
   }
 
   render() {
